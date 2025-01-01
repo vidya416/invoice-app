63277358801b098638515668b4a08d961cc7215d
Karthik <itsmeshadowmonarch@gmail.com>
2025-01-01 17:49:01 +0530

    secure routes, header and footer

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,19 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isPublic = createRouteMatcher([
+  '/'
+])
+
+// const isProtected = createRouteMatcher([
+//   '/dashboard',
+//   '/invoices/:invoiceID',
+//   '/invoices/new',
+// ])
+
+export default clerkMiddleware(async(auth, request) => {
+  if ( !isPublic(request) ) 
+    await auth.protect()
+});
 
 export const config = {
   matcher: [
