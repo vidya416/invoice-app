41a7ea9935df3808b7769c093aa259ed5702fa2c
Karthik <itsmeshadowmonarch@gmail.com>
2025-01-04 22:35:00 +0530

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,15 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+
+const isPublic = createRouteMatcher([
+  '/',
+])
+
+export default clerkMiddleware(async(auth,request)=>{
+  if ( !isPublic(request) ){
+    await auth.protect()
+  }
+});
 
 export const config = {
   matcher: [
